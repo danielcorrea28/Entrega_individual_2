5e672cd85688906332d836a1252adc80ab573128
danielcorrea28 <danielcorrea.dscc@gmail.com>
2022-09-01 15:41:35 -0500

    Creé el register y login, falta darles estilos

--- app.js
+++ app.js
@@ -15,9 +15,17 @@ app.use(express.static(path.resolve(__dirname, './public')));
 //     res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
 // });
 
-app.get('/mostrar_sitio', (req,res) =>{ 
+app.get('/', (req,res) =>{ 
     res.sendFile((__dirname + '/views/home.html'));  
 });
+
+app.get('/login', (req,res) =>{
+   res.sendFile((__dirname + '/views/login.html')) 
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile((__dirname + '/views/register.html')) 
+ });
 
 app.get('*', (req,res) =>{ 
     res.send('Ruta no definida');  // Permite enviar texto o codigo HTML
